c299e6bf781d920d2602435c5a8047c20fa38124
Laurence <ldenneulin@montesquiou.info>
2022-01-03 16:17:19 +0100

    updated Manifest.toml

--- test/test_separable_reconstruction.jl
+++ test/test_separable_reconstruction.jl
@@ -66,8 +66,8 @@ for tau in [0.03, 0.03]#, 0.07, 0.1, 0.15, 0.25, 0.5]
 
 # Linear Separable inverse method
     ML=Linear_Method(DATA, WEIGHT);
-    ML=crop(ML)
-    write(ML, "test_results/Results_Separable_Linear_$tau-$DSIZE.fits")
+    crop!(ML)
+    write(crop(ML), "test_results/Results_Separable_Linear_$tau-$DSIZE.fits")
 
 # Double Ratio    
     DR=Double_Ratio(DATA);
