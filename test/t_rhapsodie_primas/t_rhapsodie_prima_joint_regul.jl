a14558cd00e362d345a0ef248c8fb69524fc527c
Vincent Tardieux <vincent.tardieux@epita.fr>
2025-02-21 15:16:44 +0100

    Feat: now trying to fine-tune the rho in prima

--- test/t_rhapsodie_primas/t_rhapsodie_prima_joint_regul.jl
+++ test/t_rhapsodie_primas/t_rhapsodie_prima_joint_regul.jl
@@ -40,24 +40,24 @@ end
 Rhapsodie.load_data("$(root_path)DATA.fits", "$(root_path)WEIGHT.fits")
 
 function calculate_ssim_for_prima(X::Vector{Float64})
-    λ, α = X
-    regularisation_parameters = 10 .^[-8, -1., λ, -1., 0, -1.]
+    λ, α, ρ = X
+    regularisation_parameters = 10 .^[-8, -1., λ, ρ, 0, -1.]
     regularisation_parameters[5] = 0
-    println("Iteration : λ: ", λ, " α: ", α)   
+    println("Iteration : λ: ", λ, " α: ", α, " ρ: ", ρ)
     diff_fits = EasyFITS.readfits("test_results/contrast_10e$(k)/Results_Separable_DoubleDifference.fits")
     diff_polar_map = Rhapsodie.TPolarimetricMap("mixed", diff_fits[ :, :, 1]', diff_fits[ :, :, 5]', diff_fits[:, :, 2]', diff_fits[:, :, 3]')
     X0 = diff_polar_map
     x_est = apply_rhapsodie(X0, A, Rhapsodie.dataset, regularisation_parameters, maxeval=500, maxiter=1000, α=10^α, regul_type=regul_type)
     true_polar_map = Rhapsodie.read_and_fill_polar_map("mixed", "$(root_path)TRUE.fits")
     crop!(x_est)
-    write_polar_map(x_est, "test_results/prima/contrast_10e$(k)/$(regul_type)_regul/RHAPSODIE_$(λ)_$(α).fits", overwrite=true)
+    write_polar_map(x_est, "test_results/prima/contrast_10e$(k)/$(regul_type)_regul/RHAPSODIE_$(λ)_$(α)_$(ρ).fits", overwrite=true)
     curr_ssim = Rhapsodie.SSIM(x_est, true_polar_map)
-    ssim_entry = [k, λ, α, curr_ssim[8], curr_ssim[9], curr_ssim[10]]
+    ssim_entry = [k, λ, α, ρ, curr_ssim[8], curr_ssim[9], curr_ssim[10]]
     println("SSIM values: Iu_disk: ", curr_ssim[8], " | Iu_star ", curr_ssim[9])
     push!(ssim_list, ssim_entry)
     return 1 - (sum(curr_ssim[8:9]) / 2)
 end
-optimal_hyperparams, info = PRIMA.newuoa(calculate_ssim_for_prima, [-1., -1.], rhobeg=4, rhoend=1e-1, maxfun=20)
+optimal_hyperparams, info = PRIMA.newuoa(calculate_ssim_for_prima, [-1., -1., -1.], rhobeg=4, rhoend=1e-1, maxfun=20)
 io = open("test_results/prima/contrast_10e$(k)/$(regul_type)_regul/ssim.csv", "w")
 writedlm(io, header, ',')
 writedlm(io, ssim_list, ',')
